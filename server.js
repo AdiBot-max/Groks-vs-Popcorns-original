d3980fba3882684b9feb2b5b7d7be5da9aa9b19e
AdiBot-max <adichauhan12354@gmail.com>
2025-11-07 21:58:08 +0530

    FIX: serve from root

--- server.js
+++ server.js
@@ -119,5 +119,4 @@ setInterval(() => {
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`GROK vs POPCORNS LIVE AT PORT ${PORT}`);
-  console.log(`OPEN: https://groks-vs-popcorns.onrender.com`);
-});
+
